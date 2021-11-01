2f9b1480a55d92d3d3de1b5d070b88235edfc622
Noah Blumenstein <noah.blumenstein@gmail.com>
2021-11-01 18:42:30 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 10) +5;
+
+let num3 = 22;
+let num4 = 9;
+
+const mod = num3 % num4;
+
+const max = Math.max(2, 13, 20, 9, 16);
